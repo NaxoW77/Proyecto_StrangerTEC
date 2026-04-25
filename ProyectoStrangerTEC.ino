11cb1b68b086ac077612cbf12103661aff296a51
Ignacio Apuy <ignacioapuy@gmail.com>
2026-04-25 14:43:35 -0600

    Implemented shift register logic instead of direct pins

--- ProyectoStrangerTEC.ino
+++ ProyectoStrangerTEC.ino
@@ -1,31 +1,33 @@
 #include <avr/wdt.h>
 
-int led0_1 = 24;
-int led0_2 = 23;
-int led0_3 = 22;
+const int dataPin = 2;
+const int clockPin = 3;
+uint16_t ledState = 0;
 
-int led1 = 25;
-int led2 = 26;
-int led3 = 27;
-int led4 = 28;
-int led5 = 29;
-int led6 = 30;
-int led7 = 31;
-int led8 = 32;
-int led9 = 33;
-int led10 = 34;
-int led11 = 35;
-int led12 = 36;
-int led13 = 37;
+int led0_1 = 0;
+int led0_2 = 1;
+int led0_3 = 2;
+
+int led1 = 3;
+int led2 = 4;
+int led3 = 5;
+int led4 = 6;
+int led5 = 7;
+int led6 = 8;
+int led7 = 9;
+int led8 = 10;
+int led9 = 11;
+int led10 = 12;
+int led11 = 13;
+int led12 = 14;
+int led13 = 15;
 
 int leds[16] = { led0_1, led0_2, led0_3, led1, led2, led3, led4, led5, led6, led7, led8, led9, led10, led11, led12, led13 };
 
 int inputBtn = 50;
-
 int buzzer = 51;
 
-int gamemode = 0;
-
+int gamemode = 24;
 int state = 0;
 unsigned long start = 0;
 
@@ -33,10 +35,10 @@ int listen = 0;
 unsigned long execStart = 0;
 
 int letterParts[] = { -1, -1, -1, -1, -1, -1 };
-
 int letterPointer = 0;
 
 String letters[39] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-" };
+
 int morseCodes[39][6] = {
   { 0, 1, -1, -1, -1, -1 },   // A
   { 1, 0, 0, 0, -1, -1 },     // B
@@ -81,44 +83,62 @@ int morseCodes[39][6] = {
 String predefined[26] = { "ALFA", "BRAVO", "CHARLIE", "DELTA", "ECO", "FOXTROT", "GOLF", "HOTEL", "INDIA", "JULIET", "KILO", "LIMA", "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY", "XRAY", "YANKEE", "ZULU" };
 
 String selectedWord = "";
-
 String playerAWord = "";
-
 String playerBWord = "";
-
 String globalWord = "";
 
+void updateLeds() {
+  shiftOut(dataPin, clockPin, MSBFIRST, highByte(ledState));
+  shiftOut(dataPin, clockPin, MSBFIRST, lowByte(ledState));
+}
+
+void turnOn(int ledIndex) {
+  bitSet(ledState, ledIndex);
+  updateLeds();
+}
+
+void turnOff(int ledIndex) {
+  bitClear(ledState, ledIndex);
+  updateLeds();
+}
+
+void clearDisplay() {
+  ledState = 0;
+  updateLeds();
+}
 
 void preCheck() {
-  for (int x = 22; x <= 37; x++) {
+  for (int x = 0; x <= 15; x++) {
     turnOn(x);
     delay(10);
   }
   delay(200);
-  for (int x = 22; x <= 37; x++) {
+  for (int x = 0; x <= 15; x++) {
     turnOff(x);
     delay(10);
   }
   delay(100);
 
-  turnOn(buzzer);
-  for (int x = 22; x <= 37; x++) {
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, HIGH);
+  for (int x = 0; x <= 15; x++) {
     turnOn(x);
   }
 
   delay(200);
-  turnOff(buzzer);
+  digitalWrite(buzzer, LOW);
   clearDisplay();
 }
-
 
 void reset() {
   wdt_enable(WDTO_15MS);
   while (1) {}
 }
 
-
 void setup() {
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  
   preCheck();
   Serial.begin(9600);
   Serial.println("1. Arreglar problema de que si pongo un caracter invalido se salta la partida, debe más bien indicar que es invalido o no hacer nada");
@@ -128,22 +148,6 @@ void setup() {
 
   wdt_disable();
 }
-
-void clearDisplay() {
-  for (int x = 22; x <= 37; x++) {
-    turnOff(x);
-  }
-}
-
-
-void turnOn(int led) {
-  analogWrite(led, 255);
-}
-
-void turnOff(int led) {
-  analogWrite(led, 0);
-}
-
 
 void displayLetter(String letter, int sound = 0) {
   letter.toUpperCase();
@@ -156,136 +160,55 @@ void displayLetter(String letter, int sound = 0) {
     turnOn(led0_3);
     turnOn(led13);
   } else {
-
     if (row1.indexOf(letter) >= 0) {
-      turnOn(led0_1);
-
-      if (letter == "A") {
-        turnOn(led1);
-      }
-      if (letter == "B") {
-        turnOn(led2);
-      }
-      if (letter == "C") {
-        turnOn(led3);
-      }
-      if (letter == "D") {
-        turnOn(led4);
-      }
-      if (letter == "E") {
-        turnOn(led5);
-      }
-      if (letter == "F") {
-        turnOn(led6);
-      }
-      if (letter == "G") {
-        turnOn(led7);
-      }
-      if (letter == "H") {
-        turnOn(led8);
-      }
-      if (letter == "I") {
-        turnOn(led9);
-      }
-      if (letter == "J") {
-        turnOn(led10);
-      }
-      if (letter == "K") {
-        turnOn(led11);
-      }
-      if (letter == "L") {
-        turnOn(led12);
-      }
-      if (letter == "M") {
-        turnOn(led13);
-      }
+      turnOn(led0_3);
+      if (letter == "A") { turnOn(led1); }
+      if (letter == "B") { turnOn(led2); }
+      if (letter == "C") { turnOn(led3); }
+      if (letter == "D") { turnOn(led4); }
+      if (letter == "E") { turnOn(led5); }
+      if (letter == "F") { turnOn(led6); }
+      if (letter == "G") { turnOn(led7); }
+      if (letter == "H") { turnOn(led8); }
+      if (letter == "I") { turnOn(led9); }
+      if (letter == "J") { turnOn(led10); }
+      if (letter == "K") { turnOn(led11); }
+      if (letter == "L") { turnOn(led12); }
+      if (letter == "M") { turnOn(led13); }
     } else if (row2.indexOf(letter) >= 0) {
       turnOn(led0_2);
-
-      if (letter == "N") {
-        turnOn(led1);
-      }
-      if (letter == "O") {
-        turnOn(led2);
-      }
-      if (letter == "P") {
-        turnOn(led3);
-      }
-      if (letter == "Q") {
-        turnOn(led4);
-      }
-      if (letter == "R") {
-        turnOn(led5);
-      }
-      if (letter == "S") {
-        turnOn(led6);
-      }
-      if (letter == "T") {
-        turnOn(led7);
-      }
-      if (letter == "U") {
-        turnOn(led8);
-      }
-      if (letter == "V") {
-        turnOn(led9);
-      }
-      if (letter == "W") {
-        turnOn(led10);
-      }
-      if (letter == "X") {
-        turnOn(led11);
-      }
-      if (letter == "Y") {
-        turnOn(led12);
-      }
-      if (letter == "Z") {
-        turnOn(led13);
-      }
+      if (letter == "N") { turnOn(led1); }
+      if (letter == "O") { turnOn(led2); }
+      if (letter == "P") { turnOn(led3); }
+      if (letter == "Q") { turnOn(led4); }
+      if (letter == "R") { turnOn(led5); }
+      if (letter == "S") { turnOn(led6); }
+      if (letter == "T") { turnOn(led7); }
+      if (letter == "U") { turnOn(led8); }
+      if (letter == "V") { turnOn(led9); }
+      if (letter == "W") { turnOn(led10); }
+      if (letter == "X") { turnOn(led11); }
+      if (letter == "Y") { turnOn(led12); }
+      if (letter == "Z") { turnOn(led13); }
 
     } else if (row3.indexOf(letter) >= 0) {
-      turnOn(led0_3);
-
-      if (letter == "0") {
-        turnOn(led1);
-      }
-      if (letter == "1") {
-        turnOn(led2);
-      }
-      if (letter == "2") {
-        turnOn(led3);
-      }
-      if (letter == "3") {
-        turnOn(led4);
-      }
-      if (letter == "4") {
-        turnOn(led5);
-      }
-      if (letter == "5") {
-        turnOn(led6);
-      }
-      if (letter == "6") {
-        turnOn(led7);
-      }
-      if (letter == "7") {
-        turnOn(led8);
-      }
-      if (letter == "8") {
-        turnOn(led9);
-      }
-      if (letter == "9") {
-        turnOn(led10);
-      }
-      if (letter == "+") {
-        turnOn(led11);
-      }
-      if (letter == "-") {
-        turnOn(led12);
-      }
+      turnOn(led0_1);
+      if (letter == "0") { turnOn(led1); }
+      if (letter == "1") { turnOn(led2); }
+      if (letter == "2") { turnOn(led3); }
+      if (letter == "3") { turnOn(led4); }
+      if (letter == "4") { turnOn(led5); }
+      if (letter == "5") { turnOn(led6); }
+      if (letter == "6") { turnOn(led7); }
+      if (letter == "7") { turnOn(led8); }
+      if (letter == "8") { turnOn(led9); }
+      if (letter == "9") { turnOn(led10); }
+      if (letter == "+") { turnOn(led11); }
+      if (letter == "-") { turnOn(led12); }
     }
   }
 
   if (sound == 1) {
-
     int soundInd = 0;
     for (int x = 0; x < 39; x++) {
       if (letters[x] == letter) {
@@ -296,22 +219,20 @@ void displayLetter(String letter, int sound = 0) {
 
     for (int i = 0; i < 6; i++) {
       if (morseCodes[soundInd][i] == 0) {
-        turnOn(buzzer);
+        digitalWrite(buzzer, HIGH);
         delay(90);
-        turnOff(buzzer);
+        digitalWrite(buzzer, LOW);
         delay(90);
       } else if (morseCodes[soundInd][i] == 1) {
-        turnOn(buzzer);
+        digitalWrite(buzzer, HIGH);
         delay(400);
-        turnOff(buzzer);
+        digitalWrite(buzzer, LOW);
         delay(400);
       }
     }
-
     delay(500);
   }
 }
-
 
 void displayWord(String word) {
   for (int i = 0; i < word.length(); i++) {
@@ -323,18 +244,15 @@ void displayWord(String word) {
   clearDisplay();
 }
 
-
 int morseIndex(int code[6]) {
   for (int i = 0; i < 38; i++) {
     bool match = true;
-
     for (int j = 0; j < 6; j++) {
       if (morseCodes[i][j] != code[j]) {
         match = false;
         break;
       }
     }
-
     if (match) {
       return i;
     }
@@ -342,12 +260,11 @@ int morseIndex(int code[6]) {
   return -1;
 }
 
-
 void saveLetter() {
   letterPointer = 0;
   execStart = 0;
+  ledState = 0;
   clearDisplay();
-
   int ind = morseIndex(letterParts);
 
   String resultLetter = "";
@@ -366,66 +283,51 @@ void saveLetter() {
 
   Serial.print("> ");
   Serial.println(globalWord);
-
   for (int x = 0; x <= 5; x++) {
     letterParts[x] = -1;
   }
 }
 
-
 void typeWrite(String word, int speed) {
   for (int x = 0; x < word.length(); x++) {
-    turnOn(buzzer);
+    digitalWrite(buzzer, HIGH);
     Serial.print(word[x]);
     delay(speed);
-    turnOff(buzzer);
+    digitalWrite(buzzer, LOW);
     delay(speed);
   }
   Serial.println("");
 }
 
-
 String input() {
   Serial.print("> ");
-
   String result = "";
-
   while (result == "") {
-
     if (Serial.available() > 0) {
       result = Serial.readStringUntil('\n');
     }
   }
-
   Serial.println(result);
   return result;
 }
 
-
 void loop() {
-
   if (gamemode == 0) {
     Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     Serial.println("================================");
-
     typeWrite("--- BIENVENIDO A StrangerTEC ---", 50);
-
     Serial.println("================================");
     delay(1500);
     Serial.println("");
-
     gamemode = 1;
   }
 
   if (gamemode == 1) {
-
     typeWrite("Por favor, selecciona un modo de juego escribiendo su número.", 25);
-
     Serial.println("1. Modo transmisión simple.");
     Serial.println("2. Modo transmisión y escucha.");
 
     int option = input().toInt();
-
     if (option == 1) {
       gamemode = 10;
     } else if (option == 2) {
@@ -433,10 +335,8 @@ void loop() {
     } else {
       Serial.println("Error: Esa no es una opción válida.");
       gamemode = 1;
-
       delay(1000);
     }
-
     Serial.println("");
   }
 
@@ -461,11 +361,9 @@ void loop() {
       typeWrite("5. Se mostrará el puntaje de acuerdo a los aciertos por letra.", 20);
       delay(1000);
       Serial.println("");
-
       Serial.println("Escribe cualquier cosa cuando estés listo.");
       input();
     }
-
     Serial.println();
     gamemode = 21;
   }
@@ -476,12 +374,11 @@ void loop() {
     Serial.println("========================================================================");
     Serial.println("");
     typeWrite("Se ha escogido una palabra, la cual será mostrada a continuación...", 40);
-    delay(100);
+    delay(1500);
     displayWord(selectedWord);
     delay(1000);
 
     Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
     Serial.println("===========================");
     typeWrite("--- Turno del jugador A ---", 40);
     Serial.println("===========================");
@@ -495,7 +392,6 @@ void loop() {
     playerAWord = input();
     playerAWord.toUpperCase();
     Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
     gamemode = 23;
   }
 
@@ -529,7 +425,6 @@ void loop() {
     delay(1500);
 
     Serial.println("");
-
     int playerAScore = 0;
     int playerBScore = 0;
 
@@ -551,7 +446,6 @@ void loop() {
     delay(750);
 
     Serial.println("");
-
     String p1Txt = String(playerAScore);
     String p2Txt = String(playerBScore);
 
@@ -560,7 +454,7 @@ void loop() {
     typeWrite("Puntaje del jugador B: " + p2Txt + " aciertos.", 50);
     delay(1000);
     Serial.println("");
-
+    
     if (playerAScore == playerBScore) {
       typeWrite("Tenemos un empate.", 25);
     } else if (playerAScore > playerBScore) {
@@ -570,24 +464,20 @@ void loop() {
     }
 
     delay(1500);
-
     Serial.println("");
     typeWrite("Escribe cualquier cosa para comenzar otra vez.", 50);
     input();
     reset();
   }
 
-
-
   if (gamemode == 24) {
     bool val = digitalRead(inputBtn) ? 0 : 1;
+    
     if (val == 1) {
-      turnOn(buzzer);
+      digitalWrite(buzzer, HIGH);
     } else {
-      turnOff(buzzer);
+      digitalWrite(buzzer, LOW);
     }
-
-
 
     if (state == 0 && val == 1) {
       clearDisplay();
@@ -602,10 +492,11 @@ void loop() {
       unsigned long end = millis();
       unsigned long time = end - start;
       state = 0;
+      
       if (time > 65) {
         clearDisplay();
 
-        letterParts[letterPointer] = time < 300 ? 0 : 1;
+        letterParts[letterPointer] = time < 200 ? 0 : 1;
         letterPointer += 1;
 
         if (letterPointer == 6) {
@@ -618,39 +509,40 @@ void loop() {
 
         Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         Serial.print("> ");
-        Serial.print(letterParts[0] == 0 ? "." : letterParts[0] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[0] == 0 ? "." : letterParts[0] == 1 ? "-" : "");
         Serial.print(" ");
-        Serial.print(letterParts[1] == 0 ? "." : letterParts[1] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[1] == 0 ? "." : letterParts[1] == 1 ? "-" : "");
         Serial.print(" ");
-        Serial.print(letterParts[2] == 0 ? "." : letterParts[2] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[2] == 0 ? "." : letterParts[2] == 1 ? "-" : "");
         Serial.print(" ");
-        Serial.print(letterParts[3] == 0 ? "." : letterParts[3] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[3] == 0 ? "." : letterParts[3] == 1 ? "-" : "");
         Serial.print(" ");
-        Serial.print(letterParts[4] == 0 ? "." : letterParts[4] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[4] == 0 ? "." : letterParts[4] == 1 ? "-" : "");
         Serial.print(" ");
-        Serial.print(letterParts[5] == 0 ? "." : letterParts[5] == 1 ? "-"
-                                                                     : "");
+        Serial.print(letterParts[5] == 0 ? "." : letterParts[5] == 1 ? "-" : "");
         Serial.println(" ");
       }
     }
 
     if (listen == 1) {
-      turnOn(led0_1);
-      turnOn(led0_2);
-      turnOn(led0_3);
+      uint16_t tempLedState = 0;
+
+      bitSet(tempLedState, led0_1);
+      bitSet(tempLedState, led0_2);
+      bitSet(tempLedState, led0_3);
 
       for (int i = 0; i < letterPointer; i++) {
-        turnOn(leds[i + 4]);
+        bitSet(tempLedState, leds[i + 3]); 
+      }
+
+      if (tempLedState != ledState) {
+        ledState = tempLedState;
+        updateLeds();
       }
 
       unsigned long execEnd = millis();
       unsigned long execTime = execEnd - execStart;
-
+      
       if (execTime > 1000) {
         saveLetter();
         listen = 0;
